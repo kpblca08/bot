66af605e434662d0277458c64faac3a4dd950661
kpblca08 <grove.streets@yandex.ru>
2023-01-25 14:55:26 +0300

    some shit has been done

--- src/go.mod
+++
@@ -1,9 +0,0 @@
-module bot
-
-go 1.18
-
-require (
-	github.com/Syfaro/telegram-bot-api v4.6.4+incompatible // indirect
-	github.com/lib/pq v1.10.7 // indirect
-	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
-)
--- src/main.go
+++ src/main.go
@@ -3,22 +3,75 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 )
 
+type Lectures struct {
+	Lectures []Lecture `json:"Lecture"`
+}
+
 type Lecture struct {
-	Name          string `json:"name"`
-	IsWeekly      bool   `json:"isWeekly"`
-	DaysOfWeek    uint8  `json:"dayOfWeek"`
-	TimeStartMin  int    `json:"timeStart"`
-	TimeFinishMin int    `json:"timeFinish"`
-	ClassNumber   string `json:"classNumber"`
-	TeacherName   string `json:"teacherName"`
+	Name         string `json:"name"`
+	IsWeekly     uint8  `json:"isWeekly"`
+	TimeStartMin int    `json:"timeStart"`
+	ClassNumber  string `json:"classNumber"`
+	TeacherName  string `json:"teacherName"`
+}
+
+func newLecture() Lecture {
+	var L Lecture
+	fmt.Print("type name: ")
+	fmt.Scanf("%s", &L.Name)
+	fmt.Print("\n1 - every week, 2 - every 2 weeks, 0 - once: ")
+	fmt.Scanf("%d", &L.IsWeekly)
+	var hour, min int
+	var check rune
+	for {
+		fmt.Print("when it starts (hh:mm): ")
+		scanned, _ := fmt.Scanf("%d%c%d", &hour, &check, &min)
+		if (check == ':' || check == '-' || check == ' ') && (scanned == 3) {
+			break
+		}
+		fmt.Println("cringe input, try again")
+	}
+	fmt.Print("where banana: ")
+	fmt.Scanf("%s", &L.ClassNumber)
+	fmt.Print("who: ")
+	fmt.Scanf("%s", &L.TeacherName)
+	return L
+}
+
+func readInstructions(File *os.File) Lectures {
+	byteValue, _ := ioutil.ReadAll(File)
+	var Lectures Lectures
+	err := json.Unmarshal(byteValue, &Lectures)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return Lectures
 }
 
 func main() {
-	str := Lecture{"kek", true, 2, 5, 8}
-	// fmt.Print(str)
-	a, _ := json.MarshalIndent(str, "", " ")
-
+	File, err := os.Open("instructions.json")
+	if err != nil {
+		File, err = os.Create("instructions.json")
+	}
+	defer File.Close()
+	Lectures := readInstructions(File) //make([]Lecture, 10, 10)
+	lec := Lecture{
+		"Русский",
+		1,
+		720,
+		"123",
+		"Vestyak",
+	}
+	Lectures.Lectures = append(Lectures.Lectures, lec)
+	Lectures.Lectures = append(Lectures.Lectures, lec)
+	a, _ := json.MarshalIndent(Lectures, "", " ")
+	_, err = File.WriteString(string(a))
+	if err != nil {
+		fmt.Print(err)
+	}
 	fmt.Println(string(a))
 }
